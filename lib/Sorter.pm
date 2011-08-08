6de926ba92c84d02394eec7baa4044f2b80c5659
Takumi Kashima <kodokurider@gmail.com>
2011-08-09 07:13:41 +0900

    Quick Sort

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -23,38 +23,39 @@ sub get_values {
 		return @{ $self->{values} };
 }
 
-sub quick_sort {
-		my ($a,$p,$r) = @_;
-		if($p < $r){
-				my $q = &partition($a,$p,$r);
-				&quick_sort($a,$p,$q-1);
-				&quick_sort($a,$q+1,$r);
+sub partition{
+		my ($a,$l,$r) = @_;
+		my $i = $l - 1;
+		my $j = $r;
+		my $pivot = @$a[$r];
+		my $t;
+		while(1){
+				while(@$a[++$i] < $pivot){}
+				while($i < --$j && $pivot < @$a[$j]){}
+				last if($i >= $j);
+				$t = @$a[$i];
+				@$a[$i] = @$a[$j];
+				@$a[$j] = $t;
 		}
+		$t = @$a[$i];
+		@$a[$i] = @$a[$r];
+		@$a[$r] = $t;
+		return $i;
 }
 
-sub partition {
-    my ($a, $p, $r) = @_;
-    my $x = @$a[$r-1];
-    my $i = $p - 1;
-    foreach my $j (($p..$r-1)){
-				if (@$a[$j-1] <= $x) {
-						$i++;
-						my $tmp = @$a[$i-1];
-						@$a[$i-1] = @$a[$j-1];
-						@$a[$j-1] = $tmp;
-				}
-    }
-    my $tmp = @$a[$i+1-1];
-    @$a[$i+1-1] = @$a[$r-1];
-    @$a[$r-1] = $tmp;
-    return $i+1;
+sub quick_sort {
+		my ($a,$l,$r) = @_;
+		return if($l >= $r);
+		my $v = partition($a,$l,$r);
+		quick_sort($a,$l,$v-1);
+		quick_sort($a,$v+1,$r);
 }
 
 sub sort {
 		my ($self) = @_;
-		my @a = @{$self->{values}};
-		my $len = $#a+1;
-		quick_sort($self->{values},1,$len);
+		my @array = @{$self->{values}};
+		my $length =  @array;
+		quick_sort($self->{values},0,$length-1);
 }
 
 1;
